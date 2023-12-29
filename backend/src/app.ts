5003f6dc8ba6856eaae50f9f8355121280bfdad7
joseph8071 <jmoneywayy@gmail.com>
2023-12-28 23:05:59 -0700

    http demo

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middleware
-app.use(cors({ origin: "https://easepal.vercel.app", credentials: true }));
+app.use(cors({ origin: "http://easepal.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
